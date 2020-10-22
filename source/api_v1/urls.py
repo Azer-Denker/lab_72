9d4e3d27da45e87c8890ef38fc525fef0e00d19e
Aziret Arlen <azerarlen312@gmail.com>
2020-10-22 16:34:19 +0600

    API для проекта "Магазин, CRUD для товаров, просмотр и создание заказов, просмотр товаров и создание заказа - кто угодно, добавление, редактирование и удаление товаров, просмотр заказов - только сотрудники сайта (is_staff)

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -2,15 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
-from api_v1.views import ProductViewSet, OrderApi
+from api_v1.views import ProductView, OrderApiView
 
 app_name = 'api_v1'
 
 router = DefaultRouter()
-router.register(r'product', ProductViewSet)
+router.register(r'product', ProductView)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('orders/<int:pk>/', OrderApi.as_view(), name='order_api'),
+    path('orders/<int:pk>/', OrderApiView.as_view(), name='order_api'),
     path('login/', obtain_auth_token, name='api_token_auth'),
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import SAFE_METHODS, IsAdminUser
+from django.shortcuts import render, get_object_or_404
 
-from api_v1.serrializers import ProductSerializer, OrderSerializer
-from webapp.models import Product, Order
+from api_v1.serrializers import *
+from webapp.models import *
 
 
-class ProductViewSet(ModelViewSet):
+class ProductView(ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    permission_classes = [IsAdminUser]
+    serializer = ProductSerializer
+    permissions = [IsAdminUser]
 
     def get_permissions(self):
         if self.request.method in SAFE_METHODS:
@@ -19,24 +19,24 @@ class ProductViewSet(ModelViewSet):
         return super().get_permissions()
 
 
-class OrderApi(APIView):
-    permission_classes = [IsAdminUser]
-    def get(self,request,*args,**kwargs):
+class OrderApiView(APIView):
+    permissions = [IsAdminUser]
+
+    def get(self, request, *args, **kwargs):
         print(request)
         order = get_object_or_404(Order, pk=kwargs.get('pk'))
-        slr = OrderSerializer(order)
-        return Response(slr.data)
+        srl = OrderSerializer(order)
+        return Response(srl.data)
 
     def post(self, request, *args, **kwargs):
-        slr = OrderSerializer(data=request.data)
-        if slr.is_valid():
-            order = slr.save()
-            return Response(slr.data)
+        srl = OrderSerializer(data=request.data)
+        if srl.is_valid():
+            order = srl.save()
+            return Response(srl.data)
         else:
-            return Response(slr.errors, status=400)
+            return Response(srl.errors, status=400)
 
     def get_permissions(self):
         if self.request.method not in SAFE_METHODS:
             return []
         return super().get_permissions()
-
